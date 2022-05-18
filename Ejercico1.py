9e477674c1db6f262b93ad73197010b8ef57555a
David Lopez <sebastian.830@outlook.com>
2022-05-17 23:41:49 -0500

    Cambio

--- Ejercico1.py
+++ Ejercico1.py
@@ -4,8 +4,8 @@ libras = float(input("Ingresar un valor en libras: "))      #ingreso de dato
 
 kilogramo = round(libras*(1/2.2046),2)              #operacion aritmetica (de lb a kg)
 gramos = round(libras*(453.59/1),2)                  #operacion aritmetica (de lb a g)
-print("En ",libras,"lb, hay ",kilogramo,"kg")           #mostrar conversion (resultado) al usuario
-print("En ",libras,"lb, hay ",gramos,"g")
+print("En ", libras,"lb, hay ",kilogramo,"kg")           #mostrar conversion (resultado) al usuario
+print("En ", libras,"lb, hay ",gramos,"g")
 
 
 
